d56cc37df95d5e215750e436f9e1555843545c76
Ignacio <ignaeche@gmail.com>
2018-04-16 22:27:20 -0300

    before field might be null

--- app/src/main/java/com/ignaeche/reddittopposts/model/ResponseData.kt
+++ app/src/main/java/com/ignaeche/reddittopposts/model/ResponseData.kt
@@ -1,6 +1,6 @@
 package com.ignaeche.reddittopposts.model
 
 data class ResponseData(val after: String,
-                        val before: String,
+                        val before: String?,
                         val modhash: String,
                         val children: List<RedditResponse<PostData>>)
